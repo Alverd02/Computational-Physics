9a6a92876e5012b951baa2cdd36f3b113dd75de0
Albert <albert.montane02@gmail.com>
2023-10-24 13:09:31 +0200

    Practica 3

--- Practica 3/Pre-Practica 3/PrePa.f90
+++ Practica 3/Pre-Practica 3/PrePa.f90
@@ -3,23 +3,37 @@ PROGRAM P3
 IMPLICIT NONE
 
 COMMON/CONSTANTS/pi 
-DOUBLE PRECISION :: pi,valorarrel_1,fun
-INTEGER :: nitera_1
+DOUBLE PRECISION :: pi,valorarrel_1,fun,A,B,preci,valorarrel_2,valorarrel_3
+INTEGER :: nitera_1,nitera_2,nitera_3
 EXTERNAL fun
 pi = 3.1415926535898
 
 ! Apartat 2
+A = 1.5d0
+B = 2.0d0
+preci = 1.d-12
+CALL biseccio(fun,A,B,preci,nitera_1,valorarrel_1)
+WRITE(*,"(A21,I3,A18,e20.12)") "Nombre d'iteracions 1: ",nitera_1," Valor de l'arrel 1: ",valorarrel_1
 
-CALL biseccio(fun,1.5d0,2.0d0,1.d-12,nitera_1,valorarrel_1)
-WRITE(*,"(e20.12)") nitera_1,valorarrel_1
+A = 0.5d0
+B = 0.8d0
+
+CALL biseccio(fun,A,B,preci,nitera_2,valorarrel_2)
+WRITE(*,"(A21,I3,A18,e20.12)") "Nombre d'iteracions 2: ",nitera_2," Valor de l'arrel 2: ",valorarrel_2
+
+A = 0.8d0
+B = 1.0d0
+
+CALL biseccio(fun,A,B,preci,nitera_3,valorarrel_3)
+WRITE(*,"(A21,I3,A18,e20.12)") "Nombre d'iteracions 3: ",nitera_3," Valor de l'arrel 3: ",valorarrel_3
+
 END PROGRAM P3
-
 ! Apartat 1
 
 SUBROUTINE newtonraphson(fun,xini,preci,nitera,valorarrel)
-
+IMPLICIT NONE
 DOUBLE PRECISION :: xini,preci,valorarrel,x1,valordf,valorf
-INTEGER :: nitera
+INTEGER :: nitera,i
 
 
 DO i=1,999999
@@ -39,36 +53,50 @@ RETURN
 END
 
 SUBROUTINE biseccio(fun,A,B,preci,nitera,valorarrel)
-
+IMPLICIT NONE
 DOUBLE PRECISION :: A,B,preci,valorarrel,valorf_A,valorf_B,d,valordf_A,valordf_B,valorf_d,valordf_d
-INTEGER :: i,nitera
+INTEGER :: i,max_nitera,nitera
 
-nitera=INT(LOG((B-A)/preci)/LOG(2.0d0))+1
-DO i=1,nitera
-d = (A+B)/2
+max_nitera=INT(LOG((B-A)/preci)/LOG(2.0d0))+1
+
+IF (valorf_A*valorf_B.GT.0.0d0) THEN
+WRITE(*,*) "No se puede aplicar el metodo, no hay cambio de singo"
+STOP
+END IF
+
+DO i=1,max_nitera
+d = (A+B)/2.0d0
 CALL fun(A,valorf_A,valordf_A)
 CALL fun(B,valorf_B,valordf_B)
 CALL fun(d,valorf_d,valordf_d)
 
-
-IF ((valorf_A*valorf_d).LT.0.D0) THEN
-
-B = d
-
-ELSE
-
-A = d
-
+IF (valorf_d.EQ.0.0d0) THEN
+valorarrel = d
+nitera = i
+RETURN 
+STOP
 END IF
+
+IF ((valorf_A*valorf_d).LT.0.0D0) THEN
+B = d
+ELSE 
+A = d
+END IF
+
+
+IF ((B-A).LT.preci) THEN
+valorarrel = d
+nitera = i
+RETURN 
+STOP
+END IF
+
 END DO
 
-valorarrel = d
-nitera=INT(LOG((B-A)/preci)/LOG(2.0d0))+1
-RETURN 
 END
 
 SUBROUTINE fun(x,valorf,valordf)
-
+IMPLICIT NONE
 DOUBLE PRECISION :: x,valorf,valordf
 COMMON/CONSTANTS/pi 
 DOUBLE PRECISION :: pi

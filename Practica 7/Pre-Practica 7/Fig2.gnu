8fa7a55c600b1ac010250e1aef20e62d0cab19a4
Albert <albert.montane02@gmail.com>
2023-11-30 14:04:21 +0100

    Practica 7

---
+++ Practica 7/Pre-Practica 7/Fig2.gnu
@@ -0,0 +1,9 @@
+set ylabel "theta' [rad/s]"
+set xlabel "t [s]"
+set yrange [-0.1:0.15]
+
+set term png
+set output "P6-23-24-fig2.png"
+
+f(x) = -0.02*sqrt(3.71/0.45)*sin(sqrt(3.71/0.45)*x)
+plot  "P7-23-24-res.dat" index 0 using 1:2 title "Euler", f(x) title "theta' using sin(theta)=theta"
--- Practica 7/Pre-Practica 7/PrePa.f90
+++ Practica 7/Pre-Practica 7/PrePa.f90
@@ -2,68 +2,76 @@ PROGRAM P7
 
 IMPLICIT NONE
 
-DOUBLE PRECISION :: m,l,g,w_N,t_0,t_f,T_N,pi
-INTEGER :: n
+DOUBLE PRECISION :: m,l,g,w_N,t_0,t_f,T_N,pi,p_0,y_0
+DOUBLE PRECISION,DIMENSION(1300) :: p,theta
+INTEGER :: n,i
 COMMON/CONSTANTS/g,l
 EXTERNAL edo
 
 pi = 3.14159265359
 
 m = 510.d0
-l = 45.d0
+l = 0.45
 g = 3.71
 w_N = dsqrt(g/l)
 T_N = (2.d0*pi)/w_N
 t_0 = 0.d0
 t_f = 6.d0*T_N
-p_0 = 0
+
+
+
+
+OPEN(11,file="P7-23-24-res.dat")
 
 n = 1300
+y_0 = 0.02
+p_0 = 0
 
-OPEN()
+CALL euler(n,p_0,y_0,t_0,t_f,theta,p,edo)
+WRITE(11,*) "# 0)"
+DO i = 1,n
+WRITE(11,*) (t_0+(t_f-t_0)*i/n),p(i),theta(i)
+END DO
+
+WRITE(11,"(/)")
+
+n = 1800
+y_0 = pi-0.025
+
+CALL euler(n,p_0,y_0,t_0,t_f,theta,p,edo)
+WRITE(11,*) "# 1)"
+DO i = 1,n
+WRITE(11,*) (t_0+(t_f-t_0)*i/n),p(i),theta(i)
+END DO
+WRITE(11,"(/)") 
+
+CLOSE(11)
 
 END PROGRAM P7
 
-SUBROUTINE euler(n,p_0,y_0,x_0,x_f,y,funci)
+SUBROUTINE euler(n,p_0,y_0,x_0,x_f,y,p,funci)
 
 IMPLICIT NONE
 
-INTEGER :: n
-DOUBLE PRECISION,DIMENSION(n) :: y,dfunci,p
-DOUBLE PRECISION :: p_0,y_0,y_0,funci,x_0,x_f,h
+INTEGER :: n,i
+DOUBLE PRECISION,DIMENSION(n) :: y,p
+DOUBLE PRECISION :: p_0,y_0,funci,x_0,x_f,h
 
-CALL derivataula_edo(n,x_0,x_f,funci,dfunci)
-
-h = (x_1-x_0)/n
+h = (x_f-x_0)/n
 
 y(1) = y_0
 p(1) = p_0
 
 DO i = 2,n
 
-p(i) = p(i-1) + h*dfunci(i-1)
+p(i) = p(i-1) + h*funci(y(i-1))
 y(i) = y(i-1) + h*p(i-1)
 
 END DO
 
 END
 
-SUBROUTINE derivataula_edo(ndates,x_0,x_f,funci,dfunci)
-IMPLICIT NONE
-INTEGER :: ndates,i
-DOUBLE PRECISION :: h,funci,x_0,x_1,valorx
-DOUBLE PRECISION, DIMENSION(1:ndates) :: dfunci
-
-h = (x_1-x_0)/ndates
-
-DO i=1,ndates
-valorx = x_0 + i*h
-dfunci(i) = funci(valorx)
-END DO
-
-END DO
-
-DOUBLE PRECISION FUNCTION edo(y,x)
+DOUBLE PRECISION FUNCTION edo(x)
 
 IMPLICIT NONE
 
@@ -71,5 +79,5 @@ DOUBLE PRECISION :: y,x,g,l
 COMMON/CONSTANTS/g,l
 
 y = (-g/l)*dsin(x)
-
+return
 END

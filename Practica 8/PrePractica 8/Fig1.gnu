dec965c13227bb5a309d19047557868edca458d6
Albert <albert.montane02@gmail.com>
2023-12-14 17:26:05 +0100

    Practica 8

--- Practica 8/PrePractica 8/Fig1.gnu
+++ Practica 8/PrePractica 8/Fig1.gnu
@@ -1,7 +1,19 @@
-set ylabel "PHI"
-set xlabel "X"
-
 set term png
 set output "P6-23-24-fig1.png"
+set multiplot layout 2, 1  
 
-plot  "resultatsprepra8.dat" index 0,"resultatsprepra8.dat" index 1,"resultatsprepra8.dat" index 2,"resultatsprepra8.dat" index 3
+
+set key bmargin center horizontal box
+
+
+set ylabel "PHI"
+set xlabel "x"
+
+plot  "resultatsprepra8.dat" index 0 using 1:3 title"E1 400" with lines,"resultatsprepra8.dat" index 1 using 1:3 title"E2 400" with lines,"resultatsprepra8.dat" index 2 using 1:3 title"E3 400" with lines,"resultatsprepra8.dat" index 3 using 1:3 title"E4 400" with lines
+
+set ylabel "PHI"
+set xlabel "x"
+
+plot  "resultatsprepra8.dat" index 4 using 1:3 title"E1 20" with lines,"resultatsprepra8.dat" index 5 using 1:3 title"E2 20" with lines,"resultatsprepra8.dat" index 6 using 1:3 title"E3 20" with lines,"resultatsprepra8.dat" index 7 using 1:3 title"E4 20" with lines
+
+unset multiplot
--- Practica 8/PrePractica 8/PrePa.f90
+++ Practica 8/PrePractica 8/PrePa.f90
@@ -2,9 +2,9 @@ PROGRAM P8
 
 IMPLICIT NONE
 
-DOUBLE PRECISION :: pi,x0,xf,phi0,phif,V,E,E1,E2,dphi0,dx,E3,E0
-DOUBLE PRECISION,DIMENSION(2) :: funcin,dfuncout
-DOUBLE PRECISION,DIMENSION(400) :: PHI,DPHI
+DOUBLE PRECISION :: pi,x0,xf,phi0,phif,V,E,E1,E2,dphi0,dx,E3
+DOUBLE PRECISION,DIMENSION(2) :: funcin
+DOUBLE PRECISION,DIMENSION(400) :: PHI,PHI_N
 INTEGER :: nequs,N,iteracio,i,j
 COMMON/CONSTANTS/V,E
 EXTERNAL EDO
@@ -15,35 +15,54 @@ x0 = 0.0d0
 xf = 1.0d0
 phi0 = 0.0d0
 phif = 0.0d0
-dphi0 = 0.15
+dphi0 = 0.15d0
 
 V = -2.4
 
 funcin = [phi0,dphi0]
-N = 400
-dx = xf/N
+
 nequs = 2
 
 open(15,file = 'resultatsprepra8.dat')
 
+N = 400
+dx = xf/N
 
 DO i =  1,4
 
-E1 = (i*pi)**2/2.d0 + V +0.2d0
-E2 = (i*pi)**2/2.d0 + V +0.3d0
+E1 = (i*pi)**2/2.d0 + V + 0.1
+E2 = (i*pi)**2/2.d0 + V + 0.2
 
-CALL TIR(E1,E2,dx,funcin,nequs,EDO,N,E3,x0,xf,iteracio)
-E0 = E3
-CALL INTEGRAL_RK4(n,x0,xf,funcin,nequs,EDO,PHI,DPHI,E0)
+CALL TIR(E1,E2,dx,funcin,nequs,EDO,N,E3,x0,xf,iteracio,PHI)
+CALL NORM(x0,xf,PHI,PHI_N,N)
 
-WRITE(15,*) "#",E3
+WRITE(15,*) "# Iteracio = ",iteracio,"E = ",E3
+DO j = 1,N
 
-DO j =1,N
-WRITE(15,*) x0+j*dx,PHI(j)
+WRITE(15,*) x0+dx*j,PHI(j),PHI_N(j)
+
+END DO
+WRITE(15,"(/)")
 END DO
 
-WRITE(15,"(/)")
+N = 20
+dx = xf/N
 
+DO i =  1,4
+
+E1 = (i*pi)**2/2.d0 + V + 0.1
+E2 = (i*pi)**2/2.d0 + V + 0.2
+
+CALL TIR(E1,E2,dx,funcin,nequs,EDO,N,E3,x0,xf,iteracio,PHI)
+CALL NORM(x0,xf,PHI,PHI_N,N)
+
+WRITE(15,*) "# Iteracio = ",iteracio,"E = ",E3
+DO j = 1,N
+
+WRITE(15,*) x0+dx*j,PHI(j),PHI_N(j)
+
+END DO
+WRITE(15,"(/)")
 END DO
 
 CLOSE(15)
@@ -88,66 +107,122 @@ dyoutput(2) = -2.d0*(E*yinput(1)-V*yinput(1))
 
 END
 
-SUBROUTINE TIR(E1,E2,dx,funcin,nequs,edofuncio,N,E3,x_0,x_1,iteracio)
+SUBROUTINE TIR(E1,E2,dx,funcin,nequs,edofuncio,N,E3,x_0,x_1,iteracio,PHI)
 
 IMPLICIT NONE
 
-DOUBLE PRECISION,DIMENSION(nequs) :: funcin,dfuncout1,dfuncout2,dfuncout3
-DOUBLE PRECISION :: E1,E2,V,E,E3,dx,x_0,x_1,phif3,phif2,phif1
-DOUBLE PRECISION,DIMENSION(N) :: PHI1,PHI2,PHI3,DPHI1,DPHI2,DPHI3
+DOUBLE PRECISION,DIMENSION(nequs) :: funcin,funcin1,dfuncout1,dfuncout2,dfuncout3,funcin2,funcin3
+DOUBLE PRECISION,DIMENSION(N) :: PHI
+DOUBLE PRECISION :: E1,E2,V,E,E3,dx,x_0,x_1
 INTEGER :: nequs,N,i,iteracio
 COMMON/CONSTANTS/V,E 
 EXTERNAL :: edofuncio
 
 
+funcin1 = funcin
+funcin2 = funcin
+funcin3 = funcin
+
+iteracio = 1
+
+DO WHILE (iteracio.LT.N)
+
 DO i = 1,N
-
-CALL INTEGRAL_RK4(n,x_0,x_1,funcin,nequs,edofuncio,PHI1,DPHI1,E1)
-phif1 = PHI1(size(PHI1))
-CALL INTEGRAL_RK4(n,x_0,x_1,funcin,nequs,edofuncio,PHI2,DPHI2,E2)
-phif2 = PHI2(size(PHI2))
-
-
-E3 = (E1*phif2-E2*phif1)/(phif2-phif1)
-
-E=E3
-CALL INTEGRAL_RK4(n,x_0,x_1,funcin,nequs,edofuncio,PHI3,DPHI3,E3)
-phif3 = PHI3(size(PHI3))
-
-IF (DABS(phif3).LT.(1.D-5)) THEN
-E3 = E3
-iteracio = i
-EXIT
-ELSE
-E1 = E2
-E2 = E3
-
-END IF
+E = E1
+CALL RungeKutta4order(dx,funcin1,dfuncout1,nequs,edofuncio)
+funcin1 = dfuncout1
+E  = E2
+CALL RungeKutta4order(dx,funcin2,dfuncout2,nequs,edofuncio)
+funcin2 = dfuncout2
 END DO
 
+
+E3 = (E1*dfuncout2(1)-E2*dfuncout1(1))/(dfuncout2(1)-dfuncout1(1))
+
+DO i = 1,N
+
+E=E3
+PHI(i) = funcin3(1)
+CALL RungeKutta4order(dx,funcin3,dfuncout3,nequs,edofuncio)
+funcin3 = dfuncout3
+END DO
+
+IF ((DABS(dfuncout3(1))).LT.(1.d-5)) THEN
+
+E1 = E2
+E2 = E3
+iteracio = iteracio + 1
+ELSE
+
+EXIT
+
+END IF
+
+END DO
 END
 
-SUBROUTINE INTEGRAL_RK4(n,x_0,x_1,funcin,nequs,edofuncio,PHI,DPHI,E0)
+SUBROUTINE NORM(x1,x2,PHI,PHI_N,N)
 
 IMPLICIT NONE
 
-INTEGER :: i,n,nequs,j
-DOUBLE PRECISION :: dx,x_0,x_1,E0,V,E
-DOUBLE PRECISION,DIMENSION(nequs) :: funcin,dfuncout
-DOUBLE PRECISION,DIMENSION(N) :: PHI,DPHI
-COMMON/CONSTANTS/V,E
+INTEGER :: N,i
+DOUBLE PRECISION,DIMENSION(N) :: PHI_N,PHI2,PHI
+DOUBLE PRECISION :: V,E,resultat,x1,x2
+COMMON/CONSTANTS/V,E 
 EXTERNAL :: edofuncio
 
-dx = (x_1-x_0)/n
-E = E0
-DO i = 1,n
+DO i = 1,N
 
-CALL RungeKutta4order(dx,funcin,dfuncout,nequs,edofuncio)
-PHI(i) = dfuncout(1)
-DPHI(i) = dfuncout(2)
-DO j = 1,nequs
-funcin(j) = dfuncout(j)
+PHI2(i) = PHI(i)**2
+
 END DO
+
+CALL simpsontresvuit(x1,x2,n,PHI2,resultat)
+
+DO i = 1,N
+
+PHI_N(i) = PHI(i)/DSQRT(resultat)
+
 END DO
 
 END
+
+SUBROUTINE  simpsontresvuit(x1,x2,k,func,resultat)
+
+! Metode de Simpsin 3/8 repetit, utilitzem 3^k intervals i começant des de 1 fins intervals-1 
+!multipliquem per 2  o per 3 segons si i es dsivisible per 3 o no.
+
+IMPLICIT NONE
+
+DOUBLE PRECISION :: x1,x2,resultat,h,valor,x,valor0,valorN
+DOUBLE PRECISION,DIMENSION(k) :: func
+INTEGER :: intervals,k,i
+
+intervals = k
+h = (x2-x1)/intervals
+
+valor0 = func(1)
+valorN = func(size(func))
+
+resultat = valor0 + valorN
+
+DO i=1,intervals-1
+x = x1
+IF (MOD(i,3).eq.0) THEN
+
+valor =  func(i)
+resultat=resultat+2*valor
+
+ELSE
+
+valor =  func(i)
+resultat=resultat+3*valor
+
+END IF
+
+END DO
+
+resultat = (3*h*resultat)/8.
+
+RETURN
+END 
